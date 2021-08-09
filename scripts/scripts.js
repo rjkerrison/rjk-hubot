5e2a4dbe533d646f6e19389faa8b694b077d9669
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-08-09 21:15:22 +0100

    hubot updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,13 +8,21 @@
 //   None
 //
 // Commands:
-//
+//   javascript - let's the user know that the robot loves code
+//   lie - swears that the bot cannot lie
+//   hubot memorise <something> - remember something for later
+//   hubot recall - recalls the things previously memorised
+//   hubot what is your favourite <something> - replies with robot's favourite something
 
 module.exports = function (robot) {
   //  YOUR CODE HERE
   //  Example
   robot.hear(/javascript/i, function (msg) {
-    return msg.send('I love writing code!')
+    return msg.send("I love writing code! It's my favourite thing ever!")
+  })
+
+  robot.respond(/hi|hello|hey+a|howdy/i, function (msg) {
+    return msg.send('Howdy!')
   })
 
   robot.hear(/lie/i, function (msg) {
@@ -50,43 +58,3 @@ module.exports = function (robot) {
     }
   })
 }
-
-/************************************
-  
-  EXAMPLES OF THE KEY HUBOT FUNCTIONS
-  
-  ************************************/
-
-// module.exports = function(robot) {
-
-//  Example
-//  robot.hear(/javascript/i, function(msg) {
-//    return msg.send("I love writing code!");
-//  });
-
-/* Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" */
-// robot.respond(/hi|hello/i, function(msg) {
-//   return msg.send("Howdy!");
-// });
-
-/* Random Example
-    If a user asks what is your favourite x, the bot respond according to the question */
-// robot.respond(/what is your favourite (.*)/, function(msg) {
-//   let fav;
-//   fav = msg.match[1];
-//   console.log(fav);
-//   switch (fav) {
-//     case "":
-//       return msg.reply("I'm a robot--I don't eat !");
-//       break;
-//     case "band":
-//       return msg.reply("It's gotta be Daft Punk!");
-//       break;
-//     case "programming language":
-//       return msg.reply("Javascript, of course!");
-//       break;
-//     default:
-//       return msg.reply("I don't have a favourite " + fav + ". What's yours?");
-//   }
-// });
-// };
