01ed951963afed456dab6f20887f6b5db63cb6ad
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-08-09 13:34:06 +0100

    updates

---
+++ scripts/scripts.js
@@ -0,0 +1,77 @@
+// Description:
+//
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//
+
+module.exports = function (robot) {
+  //  YOUR CODE HERE
+  //  Example
+  robot.hear(/javascript/i, function (msg) {
+    return msg.send('I love writing code!')
+  })
+
+  /* Random Example
+    If a user asks what is your favourite x, the bot respond according to the question */
+  robot.respond(/what is your favourite (.*)/, function (msg) {
+    let fav
+    fav = msg.match[1]
+    console.log(fav)
+    switch (fav) {
+      case 'food':
+        return msg.reply("I'm a robot--I don't eat food!")
+      case 'band':
+        return msg.reply("It's gotta be Daft Punk!")
+      case 'programming language':
+        return msg.reply('Javascript, of course!')
+      default:
+        return msg.reply("I don't have a favourite " + fav + ". What's yours?")
+    }
+  })
+}
+
+/************************************
+  
+  EXAMPLES OF THE KEY HUBOT FUNCTIONS
+  
+  ************************************/
+
+// module.exports = function(robot) {
+
+//  Example
+//  robot.hear(/javascript/i, function(msg) {
+//    return msg.send("I love writing code!");
+//  });
+
+/* Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" */
+// robot.respond(/hi|hello/i, function(msg) {
+//   return msg.send("Howdy!");
+// });
+
+/* Random Example
+    If a user asks what is your favourite x, the bot respond according to the question */
+// robot.respond(/what is your favourite (.*)/, function(msg) {
+//   let fav;
+//   fav = msg.match[1];
+//   console.log(fav);
+//   switch (fav) {
+//     case "food":
+//       return msg.reply("I'm a robot--I don't eat food!");
+//       break;
+//     case "band":
+//       return msg.reply("It's gotta be Daft Punk!");
+//       break;
+//     case "programming language":
+//       return msg.reply("Javascript, of course!");
+//       break;
+//     default:
+//       return msg.reply("I don't have a favourite " + fav + ". What's yours?");
+//   }
+// });
+// };
